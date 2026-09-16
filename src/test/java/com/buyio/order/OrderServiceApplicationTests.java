77d153a2898505a0167b4df8af8b69571cfe4b29
Sonia Guadalupe Garcia <sonia.guadalupe.garcia@gmail.com>
2026-09-16 07:56:13 -0600

    Fix kafka

--- src/test/java/com/buyio/order/OrderServiceApplicationTests.java
+++ src/test/java/com/buyio/order/OrderServiceApplicationTests.java
@@ -2,11 +2,16 @@ package com.buyio.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean; 
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OrderServiceApplicationTests {
+
+	@MockitoBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
 	@Test
 	void contextLoads() {
