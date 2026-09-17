cb3ba8437bf4d6c8592069582bfcd7a77a562493
Sonia Guadalupe Garcia <sonia.guadalupe.garcia@gmail.com>
2026-09-17 09:48:29 -0600

    i

--- src/main/java/com/buyio/order/config/SecurityConfig.java
+++ src/main/java/com/buyio/order/config/SecurityConfig.java
@@ -1,6 +1,6 @@
-package com.empresa.order.config;
+package com.buyio.order.config;
 
-import com.empresa.order.security.JwtAuthenticationFilter;
+import com.buyio.order.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
